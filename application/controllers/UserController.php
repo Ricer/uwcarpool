470431ba1949cd5086fc3876cee1fe0ff9d3f5e4
Luke Zhao <lukeryz@icloud.com>
2014-03-08 23:24:46 -0500

    UserController

---
+++ application/controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+class UserController extends CarpoolController {
+  /**
+   * @var string
+   * main template file name, without .html
+   */
+  protected $layout = 'user';
+  
+  private $todo = array();
+
+  /**
+   * @var null
+   * sub-template file path/name, without .html
+   */
+  public $view_override = null;
+
+  public function __construct() {
+    parent::__construct();
+    session_start();
+    $this->process_url();
+  }
+  
+  public function __call($name, $arguments) {
+    $view = array();
+    $selector = array(
+      'id' => $this->todo['id']
+    );
+    $view['user'] = User::find($selector);
+    return $view;
+  }
+  
+  private function process_url(){
+    $url = explode('/', preg_replace(array('/\/+/', '/^\/|\s+|\/$/'), array('/', ''), strtolower(urldecode($_SERVER['REQUEST_URI']))));
+    if (!empty($url[1])) {
+      $this->todo['id'] = $url[1];
+    }
+  }
+}
+
+?>
